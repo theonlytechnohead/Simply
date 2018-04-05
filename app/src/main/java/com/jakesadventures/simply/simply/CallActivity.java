caf1c2eaf811ba54429fb6a10255a8cd0b030e8a
Craig Anderson <cranderson0110@gmail.com>
2018-04-06 09:14:26 +1200

    Added call activity

---
+++ app/src/main/java/com/jakesadventures/simply/simply/CallActivity.java
@@ -0,0 +1,24 @@
+package com.jakesadventures.simply.simply;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class CallActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //Remove title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //Remove notification bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_call);
+    }
+
+    public void goToHome(View view) {
+        finish();
+    }
+}
