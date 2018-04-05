8457807ba5f5368a720477efdd048a0b3ef189a6
Jakob Leech <rockyman218@gmail.com>
2018-04-05 18:10:45 +1200

    Changed adds to test

--- app/src/main/java/com/jakesadventures/simply/simply/MainActivity.java
+++ app/src/main/java/com/jakesadventures/simply/simply/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Sample AdMob app ID: ca-app-pub-3940256099942544~3347511713
-        MobileAds.initialize(this, "ca-app-pub-5066661451622533~3834896635");
+        MobileAds.initialize(this, "ca-app-pub-3940256099942544/6300978111");
         AdView mAdView = findViewById(R.id.adView);
         AdRequest adRequest = new AdRequest.Builder().build();
         mAdView.loadAd(adRequest);
