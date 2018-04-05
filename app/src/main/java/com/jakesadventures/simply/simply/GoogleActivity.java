bd572355144d570a64ae66fe0d61b86f964be1dd
Craig Anderson <cranderson0110@gmail.com>
2018-04-05 17:50:28 +1200

    Initial commit

---
+++ app/src/main/java/com/jakesadventures/simply/simply/GoogleActivity.java
@@ -0,0 +1,41 @@
+package com.jakesadventures.simply.simply;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.Button;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
+public class GoogleActivity extends AppCompatActivity {
+    private WebView googleWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_google);
+
+        googleWebView = findViewById(R.id.googleWebView);
+        WebSettings webSettings = googleWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        googleWebView.loadUrl("http://www.google.co.nz");
+        Button homeButton = findViewById(R.id.homeButton);
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.activity_main);
+            }
+        });
+    }
+
+    public void goToHome(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/jakesadventures/simply/simply/MainActivity.java
@@ -0,0 +1,30 @@
+package com.jakesadventures.simply.simply;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Sample AdMob app ID: ca-app-pub-3940256099942544~3347511713
+        MobileAds.initialize(this, "ca-app-pub-5066661451622533~3834896635");
+        AdView mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+    }
+
+    public void goToGoogle(View view) {
+        Intent intent = new Intent(this, GoogleActivity.class);
+        startActivity(intent);
+    }
+}
