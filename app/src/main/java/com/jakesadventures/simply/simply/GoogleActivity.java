3eeebe5486f44a18af6dfe39e0ff3b542b5ca354
Craig Anderson <cranderson0110@gmail.com>
2018-04-06 13:43:57 +1200

    Even more fullscreen!

--- app/src/main/java/com/jakesadventures/simply/simply/GoogleActivity.java
+++ app/src/main/java/com/jakesadventures/simply/simply/GoogleActivity.java
@@ -2,7 +2,9 @@ package com.jakesadventures.simply.simply;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -16,17 +18,27 @@ public class GoogleActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //Remove title bar
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         //Remove notification bar
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_google);
+        //getActionBar().setDisplayHomeAsUpEnabled(true);
 
         googleWebView = findViewById(R.id.googleWebView);
         WebSettings webSettings = googleWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         googleWebView.setWebViewClient(new WebViewClient());
         googleWebView.loadUrl("http://www.google.co.nz");
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public void goToHome(View view) {
--- app/src/main/java/com/jakesadventures/simply/simply/MainActivity.java
+++ app/src/main/java/com/jakesadventures/simply/simply/MainActivity.java
@@ -16,10 +16,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //Remove title bar
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         //Remove notification bar
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
         // Sample AdMob app ID: ca-app-pub-3940256099942544~3347511713
