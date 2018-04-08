ec5f1a38cd2f0fd0c7114c8a2b8cedaa4add7275
Craig Anderson <cranderson0110@gmail.com>
2018-04-08 12:51:56 +1200

    Might be able to detect incoming calls and display a toast

--- app/src/main/java/com/jakesadventures/simply/simply/OutgoingCallReceiver.java
+++ app/src/main/java/com/jakesadventures/simply/simply/OutgoingCallReceiver.java
@@ -5,20 +5,26 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.telephony.TelephonyManager;
+import android.widget.Toast;
 
 public class OutgoingCallReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        Bundle extras = intent.getExtras();
-        if (extras != null) {
-            String state = extras.getString(TelephonyManager.EXTRA_STATE);
-            if (state.equals(TelephonyManager.EXTRA_STATE_RINGING)) {
-                Intent intent1=new Intent(context, CallActivity.class);
-                intent1.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-
-                context.startActivity(intent1);
-
+        try {
+            System.out.println("Receiver start");
+            String state = intent.getStringExtra(TelephonyManager.EXTRA_STATE);
+            if(state.equals(TelephonyManager.EXTRA_STATE_RINGING)){
+                Toast.makeText(context,"Incoming call", Toast.LENGTH_SHORT).show();
             }
+            if ((state.equals(TelephonyManager.EXTRA_STATE_OFFHOOK))){
+                Toast.makeText(context,"Received call", Toast.LENGTH_SHORT).show();
+            }
+            if (state.equals(TelephonyManager.EXTRA_STATE_IDLE)){
+                Toast.makeText(context,"Idle state", Toast.LENGTH_SHORT).show();
+            }
+        }
+        catch (Exception e){
+            e.printStackTrace();
         }
     }
 }
