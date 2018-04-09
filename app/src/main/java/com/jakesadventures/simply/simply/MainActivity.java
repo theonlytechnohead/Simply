41936d6857d376a31f3e35de2f69725e40226858
Craig Anderson <cranderson0110@gmail.com>
2018-04-10 11:46:57 +1200

    Tidied comments

--- app/src/main/java/com/jakesadventures/simply/simply/MainActivity.java
+++ app/src/main/java/com/jakesadventures/simply/simply/MainActivity.java
@@ -34,22 +34,12 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-        //variables to push and pull from files
-
-
-
-        //Hide the nav bar
+        //Full screen mode
         View decorView = getWindow().getDecorView();
-        // Hide both the navigation bar and the status bar.
-        // SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
-        // a general rule, you should design your app to hide the status bar whenever you
-        // hide the navigation bar.
         int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_FULLSCREEN
                 | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
         decorView.setSystemUiVisibility(uiOptions);
-
 
         super.onCreate(savedInstanceState);
         //Remove notification bar
@@ -90,7 +80,7 @@ public class MainActivity extends AppCompatActivity {
         editor.putString("points", String.valueOf(points));
 
         //commits your edits
-        editor.commit();
+        editor.apply();
     }
 
     public void LoadInformation (){
