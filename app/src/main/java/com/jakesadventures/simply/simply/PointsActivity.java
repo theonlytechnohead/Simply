ac68a1ea4fce7ef349c4f857036d8c98432871db
Jakob Leech <rockyman218@gmail.com>
2018-04-11 10:16:00 +1200

    Cool stuff

--- app/src/main/java/com/jakesadventures/simply/simply/PointsActivity.java
+++ app/src/main/java/com/jakesadventures/simply/simply/PointsActivity.java
@@ -1,6 +1,7 @@
 package com.jakesadventures.simply.simply;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
@@ -20,6 +21,7 @@ import com.google.android.gms.ads.reward.RewardedVideoAdListener;
 
 public class PointsActivity extends Activity implements RewardedVideoAdListener {
     private RewardedVideoAd mRewardedVideoAd;
+    public int points = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +52,10 @@ public class PointsActivity extends Activity implements RewardedVideoAdListener 
         Toast.makeText(this, "onRewarded! currency: " + reward.getType() + "  amount: " +
                 reward.getAmount(), Toast.LENGTH_SHORT).show();
         // Reward the user.
+        LoadInformation();
+        points += 10;
+        SaveInformation();
+        Toast.makeText(this, "Current points: " + String.valueOf(points), Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -71,7 +77,10 @@ public class PointsActivity extends Activity implements RewardedVideoAdListener 
     @Override
     public void onRewardedVideoAdLoaded() {
         Toast.makeText(this, "onRewardedVideoAdLoaded", Toast.LENGTH_SHORT).show();
-        mRewardedVideoAd.show();
+
+        if (mRewardedVideoAd.isLoaded()) {
+            mRewardedVideoAd.show();
+        }
     }
 
     @Override
@@ -87,6 +96,24 @@ public class PointsActivity extends Activity implements RewardedVideoAdListener 
     @Override
     public void onRewardedVideoCompleted() {
         Toast.makeText(this, "onRewardedVideoCompleted", Toast.LENGTH_SHORT).show();
+    }
+
+    public void SaveInformation () {
+        // Create object of SharedPreferences.
+        SharedPreferences sharedPref= getSharedPreferences("mypref", 0);
+        //now get Editor
+        SharedPreferences.Editor editor= sharedPref.edit();
+
+        //put your value
+        editor.putString("points", String.valueOf(points));
+
+        //commits your edits
+        editor.apply();
+    }
+
+    public void LoadInformation (){
+        SharedPreferences sharedPref= getSharedPreferences("mypref", 0);
+        points = Integer.valueOf(sharedPref.getString("points", "0"));
     }
 
     @Override
