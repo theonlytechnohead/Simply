620e41b8f61732f993369712e4c41a9f1c35ca0f
Craig Anderson <cranderson0110@gmail.com>
2018-04-12 12:40:09 +1200

    What happens if we DON'T start the next activity?

--- app/src/main/java/com/jakesadventures/simply/simply/MainActivity.java
+++ app/src/main/java/com/jakesadventures/simply/simply/MainActivity.java
@@ -63,7 +63,7 @@ public class MainActivity extends AppCompatActivity {
             Intent selector = new Intent(Settings.ACTION_HOME_SETTINGS);
             showToast("Please select 'Simply' as your default home screen", 10);
             //Toast.makeText(this,"Please select 'Simply' as your default home screen", Toast.LENGTH_LONG).show();
-            startActivity(selector);
+            //startActivity(selector);
         }
     }
 
